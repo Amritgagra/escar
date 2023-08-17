ee1481fce54140636e1a6c866519855e4f9cc76c
Amrit <amritgagra200@gmail.com>
2023-08-17 15:13:15 +0530

    add conference section

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,16 @@
 
 // let target = document.querySelector(".services__cards-wrapper");
 
-//    var swiper = new Swiper(".case-slider", {
-//     autoplay:true,
-//    });
+   var swiper = new Swiper(".conference-swiper", {
+    slidesPerView: 3.4,
+    spaceBetween: 42,
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+   });
 
 //    $(window).scroll(function(){
 //     if ($(window).scrollTop() >= 50) {
