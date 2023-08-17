842c0f2df581bfdff3f72b2bc3a5b7b72fdd51fc
Amrit <amritgagra200@gmail.com>
2023-08-17 11:26:15 +0530

    project setup

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+// AOS.init();
+
+// let target = document.querySelector(".services__cards-wrapper");
+
+//    var swiper = new Swiper(".case-slider", {
+//     autoplay:true,
+//    });
+
+//    $(window).scroll(function(){
+//     if ($(window).scrollTop() >= 50) {
+//         $('.navbar').addClass('active');
+//     }
+//     else {
+//         $('.navbar').removeClass('active');
+//     }
+// });
+
+
+
+//   let observer = new IntersectionObserver(entries => {
+//       function counter(id, start, end, duration) {
+//         let obj = document.querySelector(id),
+//          current = start,
+//          range = end - start,
+//          increment = end > start ? 1 : -1,
+//          step = Math.abs(Math.floor(duration / range)),
+//          timer = setInterval(() => {
+//           current += increment;
+//           obj.textContent = current;
+//           if (current == end) {
+//            clearInterval(timer);
+//           }
+//          }, step);
+//        }
+//    counter(".counter1", 0, 17, 3000);
+//       counter(".counter2", 0, 56, 3000);
+//       counter(".counter3", 0, 300, 3000);
+//       counter(".counter4", 0, 6, 3000);
+//       counter(".counter5", 0, 155, 3000);
+//       counter(".counter6", 0, 14, 3000);
+//    });
+   
+//    observer.observe(target);
+
