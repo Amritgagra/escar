9f531d73f59f31d26ea2d6cb876ea90c299b011c
Amrit <amritgagra200@gmail.com>
2023-09-01 14:43:55 +0530

    fix issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,11 +40,3 @@
       },
    });
 
-//    $(window).scroll(function(){
-//     if ($(window).scrollTop() >= 50) {
-//         $('.navbar').addClass('active');
-//     }
-//     else {
-//         $('.navbar').removeClass('active');
-//     }
-// });
